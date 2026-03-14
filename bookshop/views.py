5504fda998813a05e1744e0a2fab91a4434fcf2b
Amelia James <ameliafjjames@gmail.com>
2026-03-14 21:10:28 +0000

    Allow customer data to be requested

--- bookshop/views.py
+++ bookshop/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from bookshop.models import Customer, Order
 
 # Create your views here.
+
+def view_orders(request):
+
+    order_list = Order.objects.all()
+
+    return render(
+        request,
+        "bookshop/orders.html",
+        {
+            "questions": order_list,
+        },
+    )
+
+def view_customer_data(request, customer_id):
+    customer = get_object_or_404(Customer, pk=customer_id)
+
+    return render(
+        request,
+        "bookshop/customer.html",
+        {
+            "customer": customer,
+        }
+    )
+
