d00b0fbefe2f85d190e1eea6bbb26847907cd024
Amelia James <ameliafjjames@gmail.com>
2026-03-15 15:49:41 +0000

    Register models with admin panel

--- bookshop/admin.py
+++ bookshop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from bookshop.models import Book, Customer, Order, OrderItem
+
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
