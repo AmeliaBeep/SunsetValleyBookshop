673f4a8397561dac154766caf497d3561d3793ce
Amelia James <ameliafjjames@gmail.com>
2026-03-15 18:11:48 +0000

    Move data building to mixin so both customer class views can inherit

--- bookshop/views.py
+++ bookshop/views.py
@@ -9,10 +9,9 @@ from django.core import serializers
 
 # Create your views here.
 
+
 def view_home(request):
-
     customer_list = Customer.objects.all()
-
     return render(
         request,
         "bookshop/home.html",
@@ -25,11 +24,19 @@ def view_home(request):
 class CustomerLookupMixin:
     def get_customer(self, customer_id):
         return get_object_or_404(Customer, pk=customer_id)
-
-
-class CustomerDataView(CustomerLookupMixin, View):
-    def get(self, request, customer_id):
+    
+    def build_customer_payload(self, customer_id):
         customer = self.get_customer(customer_id)
+        payload = {
+            "id": customer.pk,
+            "first_name": customer.first_name,
+            "last_name": customer.last_name,
+            "email": customer.email,
+            "status": customer.status,
+        }
+        return payload
+    
+    def build_orders_payload(self, customer_id):
         orders = (
             Order.objects
             .filter(customer_id=customer_id)
@@ -37,30 +44,34 @@ class CustomerDataView(CustomerLookupMixin, View):
             .prefetch_related("items__book")
         )
 
+        payload = {
+            str(order.pk): {
+                "order_id": order.pk,
+                "order_items": {
+                    str(item.pk): {
+                        "book_id": item.book_id,
+                        "book_title": item.book.title,
+                        "quantity": item.quantity,
+                        "unit_price": item.book.price,
+                        "line_total": item.quantity * item.book.price,
+                    }
+                    for item in order.items.all()
+                },
+            }
+            for order in orders
+        }
+        return payload  
+
+
+class CustomerDataView(CustomerLookupMixin, View):
+    def get(self, request, customer_id):
+        customer = self.build_customer_payload(customer_id)
+        orders = self.build_orders_payload(customer_id)
+        
+
         data = {
-            "customer": {
-                "id": customer.pk,
-                "first_name": customer.first_name,
-                "last_name": customer.last_name,
-                "email": customer.email,
-                "status": customer.status,
-            },
-            "orders": {
-                str(order.pk): {
-                    "order_id": order.pk,
-                    "order_items": {
-                        str(item.pk): {
-                            "book_id": item.book_id,
-                            "book_title": item.book.title,
-                            "quantity": item.quantity,
-                            "unit_price": item.book.price,
-                            "line_total": item.quantity * item.book.price,
-                        }
-                        for item in order.items.all()
-                    },
-                }
-                for order in orders
-            },
+            "customer": customer,
+            "orders": orders,
         }
         return JsonResponse(data)
 
@@ -71,8 +82,8 @@ class CustomerDetailView(CustomerLookupMixin, View):
     def get(self, request, customer_id):
         customer = self.get_customer(customer_id)
         return render(
-            request, 
-            self.template_name, 
+            request,
+            self.template_name,
             {
                 "customer": customer
             },
