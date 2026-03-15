73bb1cafeafe48a00ec74c062f6d0962b7092d98
Amelia James <ameliafjjames@gmail.com>
2026-03-15 00:14:29 +0000

    Add transformation of datasets into a single output

--- bookshop/views.py
+++ bookshop/views.py
@@ -1,4 +1,4 @@
-from django.db.models import CharField, F, IntegerField, Sum, Value
+from django.db.models import CharField, F, IntegerField, Sum, Value, Prefetch
 from django.db.models.functions import Coalesce, Concat
 from django.shortcuts import get_object_or_404, render
 from bookshop.models import Customer, Order
@@ -32,20 +32,26 @@ def view_customer_data(request, customer_id):
 def get_all_transformed_orders(request):
     customers=Customer.objects.filter(status='ACTIVE')
 
-    transformed_customers = customers.annotate(name=Concat(
-        "first_name", 
-        Value(" "), 
-        "last_name", 
-        output_field=CharField())
-    ).values_list('pk', 'name', 'email','status')
+
     
-    print(transformed_customers)
-    
+
+    transformed_data = (
+        Order.objects.filter(customer__status="ACTIVE")
+        .prefetch_related('customer')
+        .annotate(
+            customer_name=Concat('customer__first_name', Value(" "), 'customer__last_name'),
+            customer_email=F('customer__email'),
+            total=Coalesce(
+                Sum(F("items__quantity") * F("items__book__price"), output_field=IntegerField()),
+                0,
+            ),
+        )
+    ).values('pk', 'customer__id', 'customer_name', 'customer_email', 'total')
 
     return render(
         request,
         "bookshop/orders.html",
         {
-            "transformed_customers": transformed_customers,
+            "transformed_customers": transformed_data,
         },
     )
